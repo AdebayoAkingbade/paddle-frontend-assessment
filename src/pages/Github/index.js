c413be9db7694994f454ab97e3835881e496b0ca
adebayoakingbade <adebayo.akingbade@decagon.dev>
2022-07-17 13:27:25 +0100

    git data fetched

--- src/pages/Github/index.js
+++ src/pages/Github/index.js
@@ -15,45 +15,45 @@ const Github = () => {
   };
 
   return (
-    <div>
+    <>
       {isRepo.items &&
         isRepo.items.map((repo) => (
           <div className="repo_container" key={repo.id}>
-              {repo.owner.avatar_url && (
-            <div className="avatar">
-              <img
-                src={repo.owner.avatar_url}
-                alt=""
-                style={{ height: "80px" }}
-              />
-            </div>
-              )}
-            <div className="text">
-                {repo.name && (
-              <div className="repo_title">
-                  <span>{repo.name}</span>
+            
+              <div className="avatar">
+                <img
+                  src={repo.owner.avatar_url}
+                  alt=""
+                  style={{ height: "80px" }}
+                />
               </div>
-                )}
-                {repo.description && (
-              <span className="desc">Desc: {repo.description}</span>
-                )}
+            
+            <div className="text">
+              
+                <div className="repo_title">
+                  <span>{repo.name}</span>
+                </div>
+              
+              
+                <span className="desc">Desc: {repo.description}</span>
+              
               <div className="Starring">
-                  {repo.stargazers_count && (
-                <div className="subtitle">
-                  <i className="fa fa-star pr-1" />
-                  <span>Stars: {repo.stargazers_count}</span>
-                </div>
-                  )}
-                  {repo.open_issues_count && (
-                <div className="subtitle">
-                  <span>Issues: {repo.open_issues_count}</span>
-                </div>
-                  )}
+                
+                  <div className="subtitle">
+                    <i className="fa fa-star pr-1" />
+                    <span>Stars: {repo.stargazers_count}</span>
+                  </div>
+                
+                
+                  <div className="subtitle">
+                    <span>Issues: {repo.open_issues_count}</span>
+                  </div>
+                
               </div>
             </div>
           </div>
         ))}
-    </div>
+    </>
   );
 };
 
